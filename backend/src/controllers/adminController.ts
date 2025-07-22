ddcbba12acaf6add4329d9195555259471782973
muazarif12 <maaz.arif12@gmail.com>
2025-07-23 04:59:47 +0500

    getProducts Api implemented

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, response } from 'express';
 import { Product } from '@/models/product.model';
 import { IProduct } from '@/interfaces/IProduct';
 import { DecodedJWT } from '@/interfaces/IUser';
@@ -15,6 +15,23 @@ export const checkAdmin = async function (req: Request, res: Response, next: Nex
 
     next();
 
+}
+export const getProducts = async function (req: Request, res: Response): Promise<void> {
+
+    try {
+        const productArray: IProduct[] | null = await Product.find({})
+
+        if (!productArray || productArray.length === 0) {
+            response.status(404).json({ message: "No Products Found" })
+        }
+
+        res.status(200).json(productArray)
+
+    } catch
+    (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 export const addNewProduct = async function (req: Request, res: Response): Promise<void> {
--- backend/src/routes/admin.ts
+++ backend/src/routes/admin.ts
@@ -1,9 +1,12 @@
 import {Router } from 'express';
 import { addNewProduct, checkAdmin } from '@/controllers/adminController';
+import { getProducts } from '@/controllers/adminController';
 // import verifyJw
 export const adminRouter: Router = Router()
 
 adminRouter.use(checkAdmin)
 
 adminRouter.post("/addNewProduct",addNewProduct)
+
+adminRouter.get("/getProducts", getProducts)
 
